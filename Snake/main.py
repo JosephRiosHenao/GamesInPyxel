6d208601c0bc26b175e97aec6e169992aebe24a9
Joseph Rios Henao <joseph232005@gmail.com>
2021-02-28 15:57:17 -0500

    Comenzando Snake

---
+++ Snake/main.py
@@ -0,0 +1,15 @@
+import pyxel
+
+class SnakeGame():
+    
+    def __init__(self,width,height):
+        pyxel.run(self.update,self.draw)
+        
+    def update(self):
+        pass
+    
+    def draw(self):
+        pass
+        
+SnakeGame()
+        
