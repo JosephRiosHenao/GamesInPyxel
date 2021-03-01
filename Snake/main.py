0e622484d8fef931a61a0908a41e1939ae9ab6c7
Joseph Rios Henao <joseph232005@gmail.com>
2021-03-01 18:54:00 -0500

    Avance

--- Snake/main.py
+++ Snake/main.py
@@ -1,11 +1,16 @@
 import pyxel
 import random
+import enum
 
 WIDTH = 192
 HEIGHT = 128
 
 RESOURCES = "Assets/resources.pyxres"
-
+class Direction(enum.Enum):
+    RIGHT = 0
+    LEFT  = 1
+    DOWN  = 2
+    UP    = 3
 class Coin():
     
     def __init__(self,x,y):
@@ -18,6 +23,50 @@ class Coin():
 
     def draw(self):
         pyxel.blt(self.x,self.y,0,16,0,self.w,self.h,0)
+        
+    def randomPosition(self):
+        self.x = random.randint(0,pyxel.width)
+        self.y = random.randint(0,pyxel.height)
+        
+class SnakeSection():
+    def __init__(self,x,y,isHead=False):
+        
+        self.x = x
+        self.y = y
+        
+        self.w = 8
+        self.h = 8
+        
+        self.isHead = isHead
+        
+    def draw(self,direction):
+        
+        width = self.w
+        height = self.h
+        
+        spriteX = 0
+        spriteY = 0 
+        
+        if self.isHead:
+            
+            if direction == Direction.RIGHT:
+                spriteX = 8
+                spriteY = 8
+            if direction == Direction.LEFT:
+                spriteX = 8
+                spriteY = 8
+                width = width * -1
+                
+            if direction == Direction.UP:
+                spriteX = 0
+                spriteY = 8
+            if direction == Direction.DOWN:
+                spriteX = 0
+                spriteY = 8
+                height = height * -1
+                
+            
+        
 class App():
     
     def __init__(self,WIDHT,HEIGHT):
@@ -38,9 +87,7 @@ class App():
         
     def update(self):        
         if (pyxel.btnp(pyxel.KEY_SPACE==True)):
-            self.coin.x = random.randint(0,pyxel.width)
-            self.coin.y = random.randint(0,pyxel.height)
-
+            self.coin.randomPosition()
 
     def draw(self):
         pyxel.cls(1)
