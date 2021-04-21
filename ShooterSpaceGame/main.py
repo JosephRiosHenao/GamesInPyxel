bbf9d1a855bc10f42aed53b32dd2688733e04506
Joseph Rios Henao <joseph232005@gmail.com>
2021-04-21 09:05:32 -0500

    add effects in mouse, continue with rotate mouse dot for UIX

--- ShooterSpaceGame/main.py
+++ ShooterSpaceGame/main.py
@@ -12,12 +12,14 @@ class Game():
             fps=60
         )
         self.player = player.Player(5,3,(pyxel.width/2),(pyxel.height/2),1)
-        self.mouse = mouseObject.Mouse()
+        self.mouse = mouseObject.Mouse(3,15)
         self.keyboard = keyboardInput.KeyboardInput()
         
         pyxel.run(self.update,self.draw)
         
     def update(self):
+        
+        
         self.player.update()
         self.mouse.update()
         self.keyboard.update()
@@ -26,7 +28,7 @@ class Game():
         pyxel.cls(0)
         self.player.draw()
         self.mouse.draw()
-        pyxel.text(20,20,self.keyboard.storage,1)
+        pyxel.text((pyxel.width/2)-((pyxel.FONT_WIDTH*len(self.keyboard.storage))/2),20,self.keyboard.storage,1)
         
 if __name__ == '__main__':
     Game(192,128)
--- ShooterSpaceGame/mouseObject.py
+++ ShooterSpaceGame/mouseObject.py
@@ -1,18 +1,30 @@
 import pyxel
+import time
 
 class Mouse():
+    def __init__(self,margin,col):
+        self.col = col
+        self.colOriginal = col
+        self.margin = margin
+        self.timeReference = time.time()
     
     def update(self):
         self.x = pyxel.mouse_x
         self.y = pyxel.mouse_y
-        self.col = 15
-        self.margin = 2
+        timeActually = time.time()-self.timeReference
+        
+        if (self.col < self.colOriginal and timeActually>0.1): 
+            self.col = self.colOriginal
+            
+        if (pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON) or pyxel.btnp(pyxel.MOUSE_RIGHT_BUTTON)): 
+            self.col = 13
+            self.timeReference = time.time()
         
     def draw(self):
         pyxel.pset(self.x+self.margin,self.y,self.col) # RIGHT
         pyxel.pset(self.x-self.margin,self.y,self.col) # LEFT
         
-        pyxel.pset(self.x,self.y+self.margin,self.col) # RIGHT
-        pyxel.pset(self.x,self.y-self.margin,self.col) # LEFT
+        pyxel.pset(self.x,self.y+self.margin,self.col) # DOWN
+        pyxel.pset(self.x,self.y-self.margin,self.col) # UP
         
         pyxel.pset(self.x,self.y,self.col) # CENTER
