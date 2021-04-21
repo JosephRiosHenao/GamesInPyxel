43ab1d774784f72eef9cf90d81aa1f9937859489
Joseph Rios Henao <joseph232005@gmail.com>
2021-04-20 23:47:38 -0500

    finish the socket <3 ahhhhhhh GG I Love U

--- Sockets/Client/client.py
+++ Sockets/Client/client.py
@@ -3,51 +3,80 @@ import random
 import socket
 import threading
 
-STEPS = 1
+STEPS = 1.5
 
 class Conection():
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
         self.threadConection = threading.Thread(target=self.connect)
         self.threadConection.start()
-        self.
+        
+        self.myPos = [0,0]
+        self.otherPos = [0,0]
         
     def connect(self):
-        self.sock.connect(('localhost', 8000))
         print('Conectando...')
+        self.sock.connect(('localhost', 8000))
+        print('Conexion establecida con exito!')
         while (True):
+            # RECIBIR DATOS
             response = self.sock.recv(1024).decode()
-            response = response.split(' ')
+            response = response.split('-')
+            try:
+                self.otherPos[0] = float(response[0])
+                self.otherPos[1] = float(response[1])
+            except: 
+                try:
+                    self.otherPos[0] = float(response[1])
+                    self.otherPos[1] = float(response[2])
+                except: 
+                    print(response)
             
-            if (pyxel.btnp(pyxel.KEY_SPACE)): print(response)
+            # ENVIAR DATOS
+            if ((self.myPos[0]>0 and self.myPos[0]<pyxel.width) and (self.myPos[1]>0 and self.myPos[1]<pyxel.height) ): 
+                self.sock.send("{}-{}-".format(self.myPos[0],self.myPos[1]).encode())
+            
 
 class Cube():
-    def __init__(self,x,y,r):
-        self.x = x
-        self.y = y
-        self.r = r
+    def __init__(self,x,y):
+        self.pos = [x,y]
         self.col = random.randint(0,15)
     def draw(self):
-        pyxel.rect(self.x,self.y,self.r,self.r,self.col)
+        pyxel.rect(self.pos[0],self.pos[1],10,10,self.col)
         
 class Game():
     def __init__(self):
         pyxel.init(180,120)
-        self.player = Cube(pyxel.width/2,pyxel.height/2,2)
-        self.client = Conection()
+        self.player = Cube(pyxel.width/2,pyxel.height/2)
+        self.online = False
         pyxel.run(self.update,self.draw)
         
     def update(self):
         self.keyboardUpdate()
+        if (pyxel.btnp(pyxel.KEY_SPACE)):
+            self.client = Conection()
+            self.online = True
+            self.otherPlayer = Cube(pyxel.width/2,pyxel.height/2)
+            
+        if (self.online):
+
+            self.client.myPos[0] = self.player.pos[0]
+            self.client.myPos[1] = self.player.pos[1]  
+            
+            self.otherPlayer.pos[0] = self.client.otherPos[0]
+            self.otherPlayer.pos[1] = self.client.otherPos[1]
         
     def draw(self):
         pyxel.cls(pyxel.COLOR_LIME)
         self.player.draw()
-
+        if (self.online):
+            self.otherPlayer.draw()
+            
     def keyboardUpdate(self):
-        if (pyxel.btn(pyxel.KEY_A)): self.player.x -= STEPS
-        if (pyxel.btn(pyxel.KEY_W)): self.player.y -= STEPS
-        if (pyxel.btn(pyxel.KEY_D)): self.player.x += STEPS
-        if (pyxel.btn(pyxel.KEY_S)): self.player.y += STEPS
+        if (pyxel.btn(pyxel.KEY_A)): self.player.pos[0] -= STEPS
+        if (pyxel.btn(pyxel.KEY_W)): self.player.pos[1] -= STEPS
+        if (pyxel.btn(pyxel.KEY_D)): self.player.pos[0] += STEPS
+        if (pyxel.btn(pyxel.KEY_S)): self.player.pos[1] += STEPS
 
 Game()
--- Sockets/Server/server.py
+++ Sockets/Server/server.py
@@ -10,54 +10,69 @@ class Conection():
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind(('localhost', 8000))
         self.sock.listen(1)
-        print('Escuchando...')
+        
         self.threadConection = threading.Thread(target=self.newConexion)
         self.threadConection.start()
         
-        self.state = False
         self.myPos = [0,0]
-        
-    def update(self):
-        pass
+        self.otherPos = [0,0]
         
     def newConexion(self):
+        print('Escuchando...')
         while (True):
             conexion, addr = self.sock.accept()
             print('Nueva conexion {}'.format(addr))
-            self.state = True
             while (True):
-                conexion.send('{} {}'.format(self.myPos[0],self.myPos[1]).encode())
+                #ENVIAR DATOS
+                if ((self.myPos[0]>0 and self.myPos[0]<pyxel.width) and (self.myPos[1]>0 and self.myPos[1]<pyxel.height) ): 
+                    conexion.send("{}-{}-".format(self.myPos[0],self.myPos[1]).encode())
+                
+                # RECIBIR DATOS
+                response = conexion.recv(1024).decode()
+                response = response.split('-')
+                try:
+                    self.otherPos[0] = float(response[0])
+                    self.otherPos[1] = float(response[1])
+                except: 
+                    try:
+                        self.otherPos[0] = float(response[1])
+                        self.otherPos[1] = float(response[2])
+                    except: 
+                        print(response)
             
 class Cube():
     def __init__(self,x,y):
         self.pos = [x,y]
         self.col = random.randint(0,15)
     def draw(self):
-        pyxel.rect(self.pos[0],self.pos[1],2,2,self.col)
+        pyxel.rect(self.pos[0],self.pos[1],10,10,self.col)
         
 class Game():
     def __init__(self):
         pyxel.init(180,120)
         self.player = Cube(pyxel.width/2,pyxel.height/2)
-        self.server = Conection()
         self.online = False
         pyxel.run(self.update,self.draw)
         
     def update(self):
         self.keyboardUpdate()
-        if (self.server.state == True):
+        if (pyxel.btnp(pyxel.KEY_SPACE)):
+            self.server = Conection()
             self.online = True
-            self.server.state = False
             self.otherPlayer = Cube(pyxel.width/2,pyxel.height/2)
             
         if (self.online):
+            
             self.server.myPos[0] = self.player.pos[0]
             self.server.myPos[1] = self.player.pos[1]
+            
+            self.otherPlayer.pos[0] = self.server.otherPos[0]
+            self.otherPlayer.pos[1] = self.server.otherPos[1]
         
     def draw(self):
-        pyxel.cls(pyxel.COLOR_LIME)
+        pyxel.cls(pyxel.COLOR_LIGHTBLUE)
         self.player.draw()
-        if (self.server.state):
+        if (self.online):
             self.otherPlayer.draw()
         
     def keyboardUpdate(self):
