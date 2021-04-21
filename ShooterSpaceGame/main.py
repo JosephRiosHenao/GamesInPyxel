7de70744fe3a4f547c15ac4e68a6497b240d0910
Joseph Rios Henao <joseph232005@gmail.com>
2021-04-21 08:10:06 -0500

    create keyboard object with control the input

--- ShooterSpaceGame/main.py
+++ ShooterSpaceGame/main.py
@@ -1,3 +1,4 @@
+import keyboardInput
 import player 
 import mouseObject
 import pyxel
@@ -10,18 +11,22 @@ class Game():
             caption='ShooterGame',
             fps=60
         )
-        self.player = player.Player(10,5,(pyxel.width/2),(pyxel.height/2),1)
+        self.player = player.Player(5,3,(pyxel.width/2),(pyxel.height/2),1)
         self.mouse = mouseObject.Mouse()
+        self.keyboard = keyboardInput.KeyboardInput()
+        
         pyxel.run(self.update,self.draw)
         
     def update(self):
         self.player.update()
         self.mouse.update()
+        self.keyboard.update()
     
     def draw(self):
         pyxel.cls(0)
         self.player.draw()
         self.mouse.draw()
+        pyxel.text(20,20,self.keyboard.storage,1)
         
 if __name__ == '__main__':
     Game(192,128)
