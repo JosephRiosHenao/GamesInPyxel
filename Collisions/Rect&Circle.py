b6a31cdbab0ae1ea506cde87e36788acfdeb30ec
Joseph Rios Henao <joseph232005@gmail.com>
2021-04-26 14:51:31 -0500

    bad calibrate but work, continue to implement

--- Collisions/Rect&Circle.py
+++ Collisions/Rect&Circle.py
@@ -10,11 +10,31 @@ class Rect():
     def update(self):
         self.center = [self.pos[0]+(self.scale[0]/2),self.pos[1]+(self.scale[1]/2)]
         # if (pyxel.mouse_x>=self.pos[0] and pyxel.mouse_x<=self.pos[0]+self.scale[1]): self.per[0] = pyxel.mouse_x
-        self.per[0] = pyxel.mouse_x
-        if (pyxel.mouse_x < self.pos[0]): self.per[0] = self.pos[0]
-        if (pyxel.mouse_x > self.pos[0]): self.per[0] = self.pos[0]+self.scale[0]
+        if (pyxel.mouse_x > self.pos[0] and pyxel.mouse_x < self.pos[0] + self.scale[0]): self.per[0] = pyxel.mouse_x
+        elif (pyxel.mouse_x < self.pos[0]): self.per[0] = self.pos[0]
+        elif (pyxel.mouse_x > self.pos[0]): self.per[0] = self.pos[0]+self.scale[0]-1
+        
+        if (pyxel.mouse_y > self.pos[1] and pyxel.mouse_y < self.pos[1] + self.scale[1]): self.per[1] = pyxel.mouse_y
+        elif (pyxel.mouse_y < self.pos[1]): self.per[1] = self.pos[1]
+        elif (pyxel.mouse_y > self.pos[1]): self.per[1] = self.pos[1]+self.scale[1]-1
+        
     def draw(self):
         pyxel.rect(self.pos[0],self.pos[1],self.scale[0],self.scale[1],self.col)
+        
+    def colliding(self,pos,r):
+        # Círculo con centro en (cx,cy) y radio r
+        # Rectángulo con esquina superior izquierda en (x,y) ancho w y algo h
+        # Punto (en verde) del perímetro del rectángulo más cercano a la circunferencia en (px,py)
+        
+        self.per[0] = pos[0]
+        if ( self.per[0] < self.pos[0] ): self.per[0] = self.pos[0]
+        if ( self.per[0] > self.pos[0] + self.scale[0] ): self.per[0] = self.pos[0] + self.scale[0];
+        self.per[1] = pos[1];
+        if ( self.per[1] < self.pos[1] ): self.per[1] = self.pos[1];
+        if ( self.per[1] > self.pos[1] + self.scale[1] ): self.per[1] = self.pos[1] + self.scale[1];
+        distancia = math.sqrt( (pos[0] - self.per[0])*(pos[0] - self.per[0]) + (pos[1] - self.per[1])*(pos[1] - self.per[1]) );
+        if ( distancia < r ): self.col = 10
+        else: self.col = 7
         
 class Circle():
     def __init__(self,pos,r):
@@ -23,33 +43,22 @@ class Circle():
         self.col = 8
     def draw(self):
         pyxel.circ(self.pos[0],self.pos[1],self.r,self.col)
-    def colliding(self,pos):
-        # px = cx; // En principio son iguales
-        # if ( px < x ) px = x;
-        # if ( px > x + w ) px = x + w;
-        # py = cy;
-        # if ( py < y ) py = y;
-        # if ( py > y + h ) py = y + h;
-        # distancia = sqrt( (cx - px)*(cx - px) + (cy - py)*(cy - py) );
-        # if ( distancia < r ) {
-        #     // Colisión detectada
-        # }	
-        pass
         
 class App():
     def __init__(self):
         pyxel.init(100,80)
-        self.rect = Rect([pyxel.width/2,pyxel.height/2],[5,5])
+        self.rect = Rect([pyxel.width/2,pyxel.height/2],[10,10])
         self.circle = Circle([0,0],3)
         pyxel.run(self.update,self.draw)
     def update(self):
         self.circle.pos = [pyxel.mouse_x,pyxel.mouse_y]
         self.rect.update()
+        self.rect.colliding(self.circle.pos,self.circle.r)
     def draw(self):
         pyxel.cls(0)
         self.rect.draw()
         self.circle.draw()
-        pyxel.line(self.circle.pos[0],self.circle.pos[1],self.rect.center[0],self.rect.center[1],14)
+        # pyxel.line(self.circle.pos[0],self.circle.pos[1],self.rect.center[0],self.rect.center[1],14)
         pyxel.pset(self.rect.per[0],self.rect.per[1],14)
         
 App()
